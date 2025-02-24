1d9a612c8a02c1e14290461d7457e7451822e893
aceclub01 <acelee@dbs.com>
2025-02-24 22:48:53 +0800

    .

--- Build/AceUnityMultiPlayer.loader.js
+++ Build/AceUnityMultiPlayer.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95826419,
+    wasmFileSize: 95827905,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
