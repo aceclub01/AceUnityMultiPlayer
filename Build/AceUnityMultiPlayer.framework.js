825f67f81b48ae2751b4b1886b0d9526bc3312dd
aceclub01 <acelee@dbs.com>
2025-02-24 22:54:01 +0800

    .

--- Build/AceUnityMultiPlayer.framework.js
+++ Build/AceUnityMultiPlayer.framework.js
@@ -1284,10 +1284,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7522992: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7523047: ($0) => { performance.now = function() { return $0; }; },  
- 7523095: ($0) => { performance.now = function() { return $0; }; },  
- 7523143: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7523008: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7523063: ($0) => { performance.now = function() { return $0; }; },  
+ 7523111: ($0) => { performance.now = function() { return $0; }; },  
+ 7523159: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
